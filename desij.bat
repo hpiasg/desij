58cfa4bfe5d1967ba3c8f761a0a20fd6ec4ff65a
Stan <sgaflv@gmail.com>
2012-10-10 09:51:47 +0100

    - batch files to run the tool

--- desij.bat
+++ desij.bat
@@ -1,3 +1,3 @@
 @echo off
-set DESIJ_DIR=C:\workspace\desij
+set DESIJ_DIR=c:\workspace\desij
 java -jar %DESIJ_DIR%\desij.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ desij.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DESIJ_DIR=c:/workspace/desij
+java -jar $DESIJ_DIR/desij.jar $*
