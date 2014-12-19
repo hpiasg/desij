9432a1ba7222c05c0681a0f5c86d4bd7e3579888
Norman Kluge <norman.kluge@hpi.de>
2014-12-19 23:43:34 +0100

    added copyright and license notices

--- src/main/java/net/strongdesign/balsa/hcexpressionparser/terms/HCInfixOperator.java
+++ src/main/java/net/strongdesign/balsa/hcexpressionparser/terms/HCInfixOperator.java
@@ -1,5 +1,24 @@
 package net.strongdesign.balsa.hcexpressionparser.terms;
 
+/**
+ * Copyright 2012-2014 Stanislavs Golubcovs
+ * 
+ * This file is part of DesiJ.
+ * 
+ * DesiJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DesiJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with DesiJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
