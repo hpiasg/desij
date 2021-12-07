11fb1a76c5b73d6f1b6730010ccaaecd5217a190
Norman Kluge <norman.kluge@hpi.de>
2021-12-07 16:16:46 +0100

    remove gpl header in source files
    regex: 
    \/\*\*
     \* Copyright [\w\r\n\/() ,.:;<>*-]*?\*\/

--- src/main/java/net/strongdesign/balsa/breezefile/BreezeImport.java
+++ src/main/java/net/strongdesign/balsa/breezefile/BreezeImport.java
@@ -1,23 +1,6 @@
 package net.strongdesign.balsa.breezefile;
 
-/**
- * Copyright 2012-2014 Stanislavs Golubcovs
- * 
- * This file is part of DesiJ.
- * 
- * DesiJ is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * DesiJ is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with DesiJ.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 public class BreezeImport extends AbstractBreezeElement {
 
